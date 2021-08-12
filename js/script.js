c1be7642b39a6744b98a97df16b488e23e3feccf
Ihor <gorbenkodev@gmail.com>
2021-08-12 12:49:14 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.main-offers-slick').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    arrows: true,
+    // dots:true,
+    prevArrow: $('.slick-arrow__prev'),
+    nextArrow: $('.slick-arrow__next'),
+  });
